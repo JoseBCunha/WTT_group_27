3948ed217f047e2a5e552f576c586b904389ed9a
JoseBCunha <josebcunha2002@gmail.com>
2021-12-10 14:46:24 +0100

    Numercal 2D Polars

--- numerical_2D_polars.py
+++ numerical_2D_polars.py
@@ -11,31 +11,38 @@ Cl = polar[:,1]
 Cd = polar[:,2]
 Cm = polar[:,4]
 
+def LiftPolar():
+    plt.scatter(AoA,Cl, s=10, c="r", label="XFOIL")
+    plt.plot(AoA,Cl, color="black", linewidth=1)
+    plt.title("Cl - alpha Curve")
+    plt.xlabel("Alpha [deg]")
+    plt.ylabel("Cl")
+    plt.grid()
+    plt.legend()
+    #plt.savefig("Cl_AoA.png", dpi="figure", transparent=True)
+    return plt.show()
 
-plt.scatter(AoA,Cl, s=10, c="r")
-plt.plot(AoA,Cl, color="black", linewidth=1)
-plt.title("Cl - alpha Curve")
-plt.ylabel("Cl")
-plt.grid()
-plt.savefig("Cl_AoA.png", dpi="figure", transparent=True)
+def MomentPolar():
+    plt.scatter(AoA,Cm, s=10, c="r", label="XFOIL")
+    plt.plot(AoA,Cm, color="black", linewidth=1)
+    plt.title("Cm - alpha Curve")
+    plt.xlabel("alpha [deg]")
+    plt.ylabel("Cm")
+    plt.grid()
+    plt.legend()
+    # plt.savefig("Cm_AoA.png", dpi="figure", transparent=True)
+    return plt.show()
 
-plt.scatter(AoA,Cm, s=10, c="r")
-plt.plot(AoA,Cm, color="black", linewidth=1)
-plt.title("Cm - alpha Curve")
-plt.xlabel("alpha [deg]")
-plt.ylabel("Cm")
-plt.grid()
-plt.savefig("Cm_AoA.png", dpi="figure", transparent=True)
+def DragPolar():
+    plt.scatter(Cd,Cl, s=10, c="r", label="XFOIL")
+    plt.plot(Cd,Cl,color="black", linewidth=1)
+    plt.title("Cl - Cd Curve")
+    plt.xlabel("Cd")
+    plt.ylabel("Cl")
+    plt.grid()
+    plt.legend()
+    # plt.savefig("Cl_Cd.png", dpi="figure", transparent=True)
+    return plt.show()
 
-plt.scatter(Cd,Cl, s=10, c="r")
-plt.plot(Cd,Cl,color="black", linewidth=1)
-plt.title("Cl - Cd Curve")
-plt.xlabel("Cd")
-plt.ylabel("Cl")
-plt.grid()
-plt.savefig("Cl_Cd.png", dpi="figure", transparent=True)
-
-
-# plt.show()
 
 
