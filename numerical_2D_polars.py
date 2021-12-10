3a8ec2d1a3f672c186fbb93be6c97a813869bb3a
JoseBCunha <josebcunha2002@gmail.com>
2021-12-10 17:06:32 +0100

    Numercal 2D Polars

--- numerical_2D_polars.py
+++ numerical_2D_polars.py
@@ -12,7 +12,20 @@ Cd = polar[:,2]
 Cm = polar[:,4]
 
 # Cp data
-Cps = np.array(pd.read_csv('CP_data.csv', skiprows=1, sep=','))
+Cps = np.array(pd.read_csv('CP_data.csv', skiprows=1, sep=','))  # for alpha = -2 0 5 10 15
+def index(n):
+    if n == 0:
+        a = -2
+    elif n == 1:
+        a = 0
+    elif n == 2:
+        a = 5
+    elif n == 3:
+        a = 10
+    elif n == 4:
+        a= 15
+    return a
+
 Cp = []
 x = []
 for i in range(0, 5):
@@ -30,7 +43,7 @@ def LiftPolar():
     plt.ylabel("Cl")
     plt.grid()
     plt.legend()
-    #plt.savefig("Cl_AoA.png", dpi="figure", transparent=True)
+    plt.savefig("Cl_AoA.png", dpi="figure", transparent=True)
     return plt.show()
 
 def MomentPolar():
@@ -41,7 +54,7 @@ def MomentPolar():
     plt.ylabel("Cm")
     plt.grid()
     plt.legend()
-    # plt.savefig("Cm_AoA.png", dpi="figure", transparent=True)
+    plt.savefig("Cm_AoA.png", dpi="figure", transparent=True)
     return plt.show()
 
 def DragPolar():
@@ -52,17 +65,22 @@ def DragPolar():
     plt.ylabel("Cl")
     plt.grid()
     plt.legend()
-    # plt.savefig("Cl_Cd.png", dpi="figure", transparent=True)
+    plt.savefig("Cl_Cd.png", dpi="figure", transparent=True)
     return plt.show()
 
 def CpGraph(n):
-    plt.scatter(x[n], Cp[n], s=10, c="r", label="XFOIL")
+    name = index(n)
+    file_name = "Cp__a=%sº.png" % name
+    label_name = index(n)
+    plt.scatter(x[n], Cp[n], s=10, c="r", label=f"XFOIL for alpha = {index(n)}º")
     plt.plot(x[n], Cp[n], color="black", linewidth=1)
     plt.title("Cp Curve")
     plt.xlabel("x / c")
     plt.ylabel("Cp")
     plt.grid()
     plt.legend()
-    # plt.savefig("Cl_Cd.png", dpi="figure", transparent=True)
+    plt.savefig(file_name, dpi="figure", transparent=True)
     return plt.show()
+
+print(CpGraph(0), CpGraph(1), CpGraph(2), CpGraph(3), CpGraph(4),LiftPolar(),MomentPolar(),DragPolar())
 
