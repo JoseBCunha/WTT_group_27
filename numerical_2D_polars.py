bc0335a2ec52f61b2497dd5d0cd5de4702f30d9e
JoseBCunha <josebcunha2002@gmail.com>
2021-12-11 16:27:27 +0100

    Numercal 2D Polars

--- numerical_2D_polars.py
+++ numerical_2D_polars.py
@@ -33,14 +33,16 @@ for i in range(0, 5):
 for i in range(0, 5):
     x.append(Cps[:, 3 * i])
 
-
+# airfoil data
+airfoil = np.array(pd.read_csv("Airfoil.csv", skiprows=0, sep=","))
+x_c = airfoil[:,0]
+y_c = airfoil[:,1]
 
 def LiftPolar():
     plt.scatter(AoA,Cl, s=10, c="r", label="XFOIL")
     plt.plot(AoA,Cl, color="black", linewidth=1)
-    plt.title("Cl - alpha Curve")
-    plt.xlabel("Alpha [deg]")
-    plt.ylabel("Cl")
+    plt.xlabel("\u03B1 [deg]")
+    plt.ylabel(r"$C_{l}$")
     plt.grid()
     plt.legend()
     plt.savefig("Cl_AoA.png", dpi="figure", transparent=True)
@@ -49,20 +51,28 @@ def LiftPolar():
 def MomentPolar():
     plt.scatter(AoA,Cm, s=10, c="r", label="XFOIL")
     plt.plot(AoA,Cm, color="black", linewidth=1)
-    plt.title("Cm - alpha Curve")
-    plt.xlabel("alpha [deg]")
-    plt.ylabel("Cm")
+    plt.xlabel("\u03B1 [deg]")
+    plt.ylabel(r"$C_{m}$")
     plt.grid()
     plt.legend()
     plt.savefig("Cm_AoA.png", dpi="figure", transparent=True)
     return plt.show()
 
-def DragPolar():
+def DragPolar1():
     plt.scatter(Cd,Cl, s=10, c="r", label="XFOIL")
     plt.plot(Cd,Cl,color="black", linewidth=1)
-    plt.title("Cl - Cd Curve")
-    plt.xlabel("Cd")
-    plt.ylabel("Cl")
+    plt.xlabel(r"$C_{d}$")
+    plt.ylabel(r"$C_{l}$")
+    plt.grid()
+    plt.legend()
+    plt.savefig("Cl_Cd.png", dpi="figure", transparent=True)
+    return plt.show()
+
+def DragPolar2():
+    plt.scatter(AoA,Cd, s=10, c="r", label="XFOIL")
+    plt.plot(AoA,Cd,color="black", linewidth=1)
+    plt.xlabel("\u03B1 [deg]")
+    plt.ylabel(r"$C_{d}$")
     plt.grid()
     plt.legend()
     plt.savefig("Cl_Cd.png", dpi="figure", transparent=True)
@@ -71,16 +81,23 @@ def DragPolar():
 def CpGraph(n):
     name = index(n)
     file_name = "Cp__a=%sº.png" % name
-    label_name = index(n)
     plt.scatter(x[n], Cp[n], s=10, c="r", label=f"XFOIL for alpha = {index(n)}º")
     plt.plot(x[n], Cp[n], color="black", linewidth=1)
-    plt.title("Cp Curve")
-    plt.xlabel("x / c")
-    plt.ylabel("Cp")
+    plt.xlabel("x/c")
+    plt.ylabel(r"$C_{p}$")
     plt.grid()
     plt.legend()
     plt.savefig(file_name, dpi="figure", transparent=True)
     return plt.show()
 
-print(CpGraph(0), CpGraph(1), CpGraph(2), CpGraph(3), CpGraph(4),LiftPolar(),MomentPolar(),DragPolar())
+def Airfoil():
+    plt.figure(figsize=(16, 3))
+    plt.scatter(x_c,y_c, s=10, c="r")
+    plt.plot(x_c,y_c, color="black", linewidth=1)
+    plt.xlabel("x/c")
+    plt.ylabel("y/c")
+    plt.savefig("airfoil_plot.png", dpi="figure", transparent=True)
+    return plt.show()
+
+# print(CpGraph(0), CpGraph(1), CpGraph(2), CpGraph(3), CpGraph(4),LiftPolar(),MomentPolar(),DragPolar(),DragPolar2(),Airfoil())
 
