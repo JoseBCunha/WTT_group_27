7986849d786826b09967c12810d13e38bd397ebd
JoseBCunha <josebcunha2002@gmail.com>
2021-12-12 23:16:51 +0100

    Numercal 2D Polars

--- numerical_2D_polars.py
+++ numerical_2D_polars.py
@@ -4,12 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# polar data
+# Viscous polar data
 polar = np.array(pd.read_csv('polar_data.csv', skiprows=1, sep=','))
-AoA = polar[:,0]
-Cl = polar[:,1]
-Cd = polar[:,2]
-Cm = polar[:,4]
+AoA = polar[:, 0]
+Cl = polar[:, 1]
+Cd = polar[:, 2]
+Cm = polar[:, 4]
+
+# Inviscid polar data
+invisc_polar = np.array(pd.read_csv("invisc_polars.csv", skiprows=1, sep=","))
+in_AoA = invisc_polar[:26, 0]
+in_Cl = invisc_polar[:26, 1]
+
 
 # Cp data
 Cps = np.array(pd.read_csv('CP_data.csv', skiprows=1, sep=','))  # for alpha = -2 0 5 10 15
@@ -38,6 +44,7 @@ airfoil = np.array(pd.read_csv("Airfoil.csv", skiprows=0, sep=","))
 x_c = airfoil[:,0]
 y_c = airfoil[:,1]
 
+# plots
 def LiftPolar():
     plt.scatter(AoA,Cl, s=10, c="r", label="XFOIL")
     plt.plot(AoA,Cl, color="black", linewidth=1)
@@ -100,4 +107,18 @@ def Airfoil():
     plt.savefig("airfoil_plot.png", dpi="figure", transparent=True)
     return plt.show()
 
+def Compare_LiftPolar():
+    plt.scatter(in_AoA,in_Cl, s=18, c="g", label="Inviscid", marker="^")
+    plt.plot(in_AoA,in_Cl, color="black", linewidth=1)
+    plt.scatter(AoA,Cl, s=10, c="r", label="Viscous")
+    plt.plot(AoA,Cl, color="black", linewidth=1)
+    plt.xlabel("\u03B1 [deg]")
+    plt.ylabel(r"$C_{l}$")
+    plt.grid()
+    plt.legend()
+    plt.savefig("Invisc_Cl_AoA.png", dpi="figure", transparent=True)
+    return plt.show()
+
+
 # print(CpGraph(0), CpGraph(1), CpGraph(2), CpGraph(3), CpGraph(4),LiftPolar(),MomentPolar(),DragPolar(),DragPolar2(),Airfoil())
+print(Compare_LiftPolar())
